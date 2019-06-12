2f79dae24bec14e9ed4fb4c613fddc4ade42e8da
Truong Ma Phi <trmaphi@gmail.com>
2019-06-12 22:56:41 +0700

    Update .gitconfig

--- functions/set-local-git-config.sh
+++ functions/set-local-git-config.sh
@@ -1,9 +1,11 @@
 set-git-config-work() {
 	git config user.name "trmaphi"
 	git config user.email "phitruong@lecle.co.kr"
+	git config core.editor "code --wait"
 }
 
 set-git-config-personal() {
 	git config user.name "trmaphi"
 	git config user.email "trmaphi@gmail.com"
+	git config core.editor "code --wait"
 }
