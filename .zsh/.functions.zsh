579f382b05cf74b7c9f771bb67c8d0dc649627f2
trmaphi <trmaphi@gmail.com>
2019-11-12 14:55:24 +0700

    Fzf history brow: edit before execute

--- .zsh/.functions.zsh
+++ .zsh/.functions.zsh
@@ -1,4 +1,5 @@
 # https://github.com/junegunn/fzf/wiki/examples#command-history
+# fh - repeat history
 fh() {
-  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -r 's/ *[0-9]*\*? *//' | sed -r 's/\\/\\\\/g')
 }
