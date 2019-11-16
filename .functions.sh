1a67d1b6fe89362355f0f06b366f668ac16ab0ae
trmaphi <trmaphi@gmail.com>
2019-11-16 18:08:55 +0700

    Add fuzzy checkout

--- .functions.sh
+++ .functions.sh
@@ -1,37 +1,37 @@
 # Darwin
-if [ $(uname) = "Darwin" ] ; then
-	# Add Visual Studio Code (code)
-	export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+if [ $(uname) = "Darwin" ]; then
+  # Add Visual Studio Code (code)
+  export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-	# Load linux vm in Docker toolbox
-	load-docker-toolbox(){
-		. ${HOME}/.scripts/.docker-tool-box
-	}
+  # Load linux vm in Docker toolbox
+  load-docker-toolbox() {
+    . ${HOME}/.scripts/.docker-tool-box
+  }
 fi
 
 set-git-config-personal() {
-	git config user.name "trmaphi"
-	git config user.email "trmaphi@gmail.com"
-	git config core.editor "vim"
+  git config user.name "trmaphi"
+  git config user.email "trmaphi@gmail.com"
+  git config core.editor "vim"
 }
 
 get-git-current-remote-branch() {
-	git rev-parse --abbrev-ref --symbolic-full-name @{u}
+  git rev-parse --abbrev-ref --symbolic-full-name @{u}
 }
 
 stop-all-docker-container() {
-	docker stop $(docker ps -aq)
+  docker stop $(docker ps -aq)
 }
 
 rm-all-docker-container() {
-	docker rm $(docker ps -aq)
+  docker rm $(docker ps -aq)
 }
 
 # fshow - git commit browser
 fshow() {
   git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+    --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
@@ -43,9 +43,9 @@ FZF-EOF"
 fbrach() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    branch=$(echo "$branches" |
+      fzf-tmux -d $((2 + $(wc -l <<<"$branches"))) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
 # get git commit sha
@@ -53,42 +53,52 @@ fbrach() {
 fgitsha() {
   local commits commit
   commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
-  echo -n $(echo "$commit" | sed "s/ .*//")
+    commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+    echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
 # fcoc - checkout git commit
 fcheckoutcommit() {
   local commits commit
   commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
-  git checkout $(echo "$commit" | sed "s/ .*//")
+    commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+    git checkout $(echo "$commit" | sed "s/ .*//")
 }
 
-# ftags - search ctags
-ftags() {
-  local line
-  [ -e tags ] &&
-  line=$(
-    awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-    cut -c1-80 | fzf --nth=1,2
-  ) && ${EDITOR:-vim} $(cut -f3 <<< "$line") -c "set nocst" \
-                                      -c "silent tag $(cut -f2 <<< "$line")"
+# fco - checkout git branch/tag
+fcheckout() {
+  local tags branches target
+  branches=$(
+    git --no-pager branch --all \
+      --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
+      sed '/^$/d'
+  ) || return
+  tags=$(
+    git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
+  ) || return
+  target=$(
+    (
+      echo "$branches"
+      echo "$tags"
+    ) |
+      fzf --no-hscroll --no-multi -n 2 \
+        --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
+  ) || return
+  git checkout $(awk '{print $2}' <<<"$target")
 }
 
 # fkill - kill processes - list only the ones you can kill. Modified the earlier script.
 fkill() {
-    local pid
-    if [ "$UID" != "0" ]; then
-        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-    else
-        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-    fi
+  local pid
+  if [ "$UID" != "0" ]; then
+    pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+  else
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  fi
 
-    if [ "x$pid" != "x" ]
-    then
-        echo $pid | xargs kill -${1:-9}
-    fi
+  if [ "x$pid" != "x" ]; then
+    echo $pid | xargs kill -${1:-9}
+  fi
 }
 
 # Select a docker image to start and attach to
