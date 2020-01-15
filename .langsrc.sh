a111b8b7aa4fc8fb6a537b89bf5b7c6551f7b6b1
trmaphi <trmaphi@gmail.com>
2020-01-15 13:03:58 +0700

    refactor: add languages tools for python

--- .langsrc.sh
+++ .langsrc.sh
@@ -1,9 +1,12 @@
-# # Set path rust cargo
+# Set path rust cargo
 # export PATH="$HOME/.cargo/bin:$PATH"
 
-# # Set golang GO_PATH environment variable
+# Set golang GO_PATH environment variable
 # export GOPATH=$(go env GOPATH)
 
 # Set NVM home and load nvm
 export NVM_DIR="${HOME}/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+
+# Set python --user install bin
+export PATH="$(python -m site --user-base)/.bin:$PATH"
