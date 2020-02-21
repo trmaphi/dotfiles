3038254dbeb64b827f2269f15766e9ea55fecae2
trmaphi <trmaphi@gmail.com>
2020-02-21 23:51:37 +0700

    chore: add function keyword

--- .functions.sh
+++ .functions.sh
@@ -3,14 +3,10 @@ function set-git-config-personal() {
   git config core.editor "vim"
 }
 
-get-git-current-remote-branch() {
-  git rev-parse --abbrev-ref --symbolic-full-name @{u}
-}
-
-stop-all-docker-container() {
+function stop-all-docker-container() {
   docker stop $(docker ps -aq)
 }
 
-rm-all-docker-container() {
+function rm-all-docker-container() {
   docker rm $(docker ps -aq)
 }
