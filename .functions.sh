14dffba1d38609eea7675df90a0ec5f9fc318c6c
trmaphi <trmaphi@gmail.com>
2020-02-21 23:50:15 +0700

    refactor: use binary instead of function

--- .functions.sh
+++ .functions.sh
@@ -1,10 +1,4 @@
-# Load linux vm in Docker toolbox
-load-docker-toolbox() {
-  . ${HOME}/.scripts/.docker-tool-box
-}
-
-set-git-config-personal() {
-  git config user.name "trmaphi"
+function set-git-config-personal() {
   git config user.email "trmaphi@gmail.com"
   git config core.editor "vim"
 }
